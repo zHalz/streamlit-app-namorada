6b249aad7d7040431ec35027f72fabbd9326610b
Raul <Raul.live04@gmail.com>
2026-03-19 21:34:38 -0300

    criando paginas iniciais

--- app.py
+++ app.py
@@ -1,4 +1,24 @@
+import streamlit as st
+from components.header import render_header
 from streamlit_option_menu import option_menu
+
+st.set_page_config(layout="wide")
+
+# carregar css
+def load_css():
+    with open("assets/style.css") as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+load_css()
+
+render_header()
+
+st.markdown("""
+<div class="hero">
+    <h1>Bem-vinda ❤️</h1>
+    <p>Esse espaço foi feito com carinho pra você</p>
+</div>
+""", unsafe_allow_html=True)
 
 selected = option_menu(
     menu_title=None,
