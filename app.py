1a5025499b486ce28241242d78999b987addf656
Raul <Raul.live04@gmail.com>
2026-03-19 21:12:16 -0300

    estrutura inicial correta

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Nosso App ❤️",
+    layout="wide"
+)
+
+st.title("Bem-vinda ❤️")
+st.write("Esse app foi feito especialmente pra você")
