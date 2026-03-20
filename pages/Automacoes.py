626aacda8a7855b6b5e3176726bbf53dab52ba0c
Raul <Raul.live04@gmail.com>
2026-03-19 21:27:42 -0300

    criando paginas iniciais

---
+++ pages/Automacoes.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("⚙️ Automações")
+
+st.write("Aqui vão ficar nossas ferramentas inteligentes 🚀")
+
+if st.button("Teste"):
+    st.success("Funcionando!")
---
+++ pages/Especial.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("❤️ Um espaço especial")
+
+st.write("Essa parte é só nossa 😊")
+
+st.balloons()
