a62d38d475ec00f543c2f52ae1744fc2c6b360b1
Raul <Raul.live04@gmail.com>
2026-03-19 21:22:18 -0300

    home inicial com css

--- app.py
+++ app.py
@@ -5,5 +5,16 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("Bem-vinda ❤️")
-st.write("Esse app foi feito especialmente pra você")
+# carregar css
+def load_css():
+    with open("assets/style.css") as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+load_css()
+
+st.markdown("""
+<div class="hero">
+    <h1>Bem-vinda ❤️</h1>
+    <p>Esse espaço foi feito com carinho pra você</p>
+</div>
+""", unsafe_allow_html=True)
